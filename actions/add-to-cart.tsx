b3a4473c7fd701b91cff2e8a7d2f75080bdb279b
Durgesh Prajapati <drgaming143@gmail.com>
2024-03-12 11:38:12 +0530

    removed Addto Cart file due to uncessory use

--- actions/add-to-cart.tsx
+++ actions/add-to-cart.tsx
@@ -1,15 +1,15 @@
-import { OrderItem } from "@/types";
-import axios from "axios";
+// import { OrderItem } from "@/types";
+// import axios from "axios";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/orderItems`;
+// const URL = `${process.env.NEXT_PUBLIC_API_URL}/orderItems`;
 
-const addProductToCart = async (id: string) => {
-  const res = await axios.post(`${URL}/${id}`, data: {
-    id
-  });
+// const addProductToCart = async (id: string) => {
+//   const res = await axios.post(`${URL}/${id}`, data: {
+//     id
+//   });
   
-  console.log(res.json());
-	// return res.json();
-};
+//   console.log(res.json());
+// 	// return res.json();
+// };
 
-export default addProductToCart;
+// export default addProductToCart;
